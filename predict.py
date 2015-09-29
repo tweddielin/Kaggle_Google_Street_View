80487db6ab5795fab112e3ba804df9b14fd540f8
Ubuntu <ubuntu@ip-172-31-5-110.ec2.internal>
2015-09-29 03:52:44 +0000

    updte predict

--- predict.py
+++ predict.py
@@ -1,18 +1,30 @@
 import cPickle
 import numpy as np
-from load import make_result_csv
+import csv
+
+test_file_name = open('test_file_name.cpickle').read()
+test_file_name = cPickle.loads(test_file_name)
 
 predict = open('predict_cnn.cpickle').read()
 predict = cPickle.loads(predict)
 
 classes = open('classes.cpickle').read()
 classes = cPickle.loads(classes)
-
-predict = predict.astype(np.int32)
+print classes
+#predict = predict.astype(np.int32)
 
 
 index = np.argmax(predict, axis=1)
 
 result = [classes[i] for i in index]
+print result[:10]
 
-make_result_csv(result, 'result_cnn')
+with open('result_cnn.csv','w') as csvfile:
+	fieldnames = ['ID', 'Class']
+	writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+	writer.writeheader()
+
+	for f in range(len(result)):
+		writer.writerow({'ID': test_file_name[f], 'Class': result[f]})
+
+
